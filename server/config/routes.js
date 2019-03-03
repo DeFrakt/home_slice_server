a82b75dc0616b51806807c3d3ddfb174587f6a18
DeFrakt <syaussi@gmail.com>
2019-03-02 20:22:03 -0800

    routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -5,8 +5,8 @@ module.exports = function(app){
     app.post('/api/cc', function(req, res) {
         ccTransaction.add(req, res);
     })
-    //point to Angular index
-    // app.all('*', function(req, res) {
-    //    res.sendFile(path.resolve('./public/dist/public/index.html'));
-    // })
+    // point to Angular index
+    app.all('*', function(req, res) {
+       res.sendFile(path.resolve('./public/dist/public/index.html'));
+    })
 }
